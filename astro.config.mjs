ae775d6d3eb19d229a2a288dcd0757aad4a96629
Yaroslav Rogovich <yrogovich@gmail.com>
2023-08-26 19:39:26 +0200

    build: config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yrogovich.github.io',
+  base: '/polish-modernism',
   output: 'static',
   vite: {
     css: {
