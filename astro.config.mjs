7c84fed83d87c0bbbbe25d8d14f609711503dece
Yaroslav <yrogovich@gmail.com>
2023-08-28 00:49:12 +0200

    Revert "feat: rebuild component with react"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yrogovich.github.io',
@@ -17,6 +15,5 @@ export default defineConfig({
         }
       }
     }
-  },
-  integrations: [react()]
+  }
 });
