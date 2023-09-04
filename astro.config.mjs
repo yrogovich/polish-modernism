ca70ee02bb98aedbb26841ebf0d3b9276df6a8bd
Yaroslav Rogovich <yrogovich@gmail.com>
2023-09-04 16:48:17 +0300

    chore: unpdate Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yrogovich.github.io',
-  base: '/polish-modernism',
-  experimental: {
-    assets: true
-  },
   vite: {
     css: {
       preprocessorOptions: {
