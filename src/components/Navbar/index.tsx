11e40e7ab99a748f108ca50f8c47532784704c00
Yaroslav Rogovich <yrogovich@gmail.com>
2023-09-04 16:19:42 +0300

    feat: add background content

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react'
 import styles from './styles.module.scss'
 
 const Navbar = () => {
+  const [navbarHeight, setNavbarHeight] = useState(null);
+  const navbarRef = useRef(null);
+
+  useEffect(() => {
+    setNavbarHeight(navbarRef.current.clientHeight);
+  }, [navbarRef])
+
   return (
-    <div className={styles.navbar}>
-      <div className={styles.navbar__label}>Collection <br/> of Polish modernism</div>
-      <div className={styles.navbar__label}>About us</div>
-      <div className={styles.navbar__label}>Show Grid</div>
-    </div>
+    <>
+      <div className={styles.navbar} ref={navbarRef}>
+        <div className={styles.navbar__label}>Collection <br/> of Polish modernism</div>
+        <div className={styles.navbar__label}>About us</div>
+        <div className={styles.navbar__label}>Show Grid</div>
+      </div>
+      <div style={{height: `${navbarHeight}px`}}></div>
+    </>
   );
 };
 
