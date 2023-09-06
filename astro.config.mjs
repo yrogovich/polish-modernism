2b200e2d67f6f1d2fac5eb1b17af13a23667d798
Yaroslav Rogovich <yrogovich@gmail.com>
2023-09-06 22:21:22 +0200

    chore: add nanostores

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yrogovich.github.io',
-  base: '/polish-modernism',
+  base: 'polish-modernism/',
   vite: {
     css: {
       preprocessorOptions: {
