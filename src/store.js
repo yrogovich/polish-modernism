d29aed96c84b42340f2ac0582e1b16d24029120e
Yaroslav Rogovich <yrogovich@gmail.com>
2023-10-01 22:51:05 +0200

    refactor: update assets

--- src/store.js
+++ src/store.js
@@ -5,4 +5,4 @@ export const navHeight = atom(0);
 export const isFooterInView = atom(false);
 export const isPopupOpen = atom(false);
 export const isMobileMenuOpen = atom(false);
-export const isDevModeOn = atom(true);
+export const isDevModeOn = atom(false);
