1e9e77ae78945dedc772d820dde86f4747570f0e
Yaroslav Rogovich <yrogovich@gmail.com>
2023-10-07 13:38:16 +0200

    style: improve Popup animation

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -36,19 +36,71 @@ const Popup = () => {
 
   const renderAuthor = (author, index) => (
     <motion.div className={styles.popup__author} key={`author-${index}`}>
-      <motion.div className={styles.popup__authorRole}>
+      <motion.div
+        className={styles.popup__authorRole}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          type: "easeInOut",
+          duration: 0.6,
+          delay: 1 * (index / 2),
+        }}
+      >
         {author.role}
       </motion.div>
       <motion.a
         href={author.socials[0].link}
         className={styles.popup__authorImage}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          type: "linear",
+          duration: 0.8,
+          delay: 0.5 * (index / 2),
+        }}
       >
         <img src={author.image.src} alt={author.name} loading={"lazy"} />
       </motion.a>
-      <motion.div className={styles.popup__authorName}>
+      <motion.div
+        className={styles.popup__authorName}
+        initial={{
+          opacity: 0,
+          y: 50,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          type: "linear",
+          duration: 0.5,
+          delay: 0.7 * (index / 2),
+        }}
+      >
         {author.name}
       </motion.div>
-      <motion.div className={styles.popup__authorSocials}>
+      <motion.div
+        className={styles.popup__authorSocials}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          type: "easeInOut",
+          duration: 0.5,
+          delay: 0.6 * index,
+        }}
+      >
         {author.socials.map((social) => (
           <motion.a
             href={social.link}
@@ -84,6 +136,7 @@ const Popup = () => {
                 y: "100%",
               }}
               transition={{
+                stagger: 0.5,
                 duration: 0.6,
                 type: "easeInOut",
               }}
@@ -92,7 +145,9 @@ const Popup = () => {
             >
               <motion.div className={styles.popup__content}>
                 <motion.div className={styles.popup__row}>
-                  {authors.map((author, index) => renderAuthor(author, index))}
+                  {authors.map((author, index) =>
+                    renderAuthor(author, index + 1),
+                  )}
                 </motion.div>
               </motion.div>
             </motion.div>
